e6124883248bd47ef9d2e9876d11fd18e21ce70c
TiagoMPerin <tiagomartinsperin@gmail.com>
2026-05-05 11:57:20 -0300

    alterações no código

---
+++ src/utils/produtoUtils.js
@@ -0,0 +1,8 @@
+import { moedaPais, idioma } from "./global"
+function formataPreco(preco){
+    return Number(preco).toLocaleString(idioma, {
+        style: 'currency',
+        currency: moedaPais
+    })
+}
+export { formataPreco }
